d8cd217be1de6b6af0a59a958998d3e193c80e6d
gianniemann <gian.niemann@gmail.com>
2021-11-16 13:59:13 +0100

    Added button save + open new layout on click

--- app/src/main/java/com/example/myflight/MainActivity.java
+++ app/src/main/java/com/example/myflight/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.myflight;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+  /**Button btnHit;
+    TextView txtJson;
+    ProgressDialog pd;**/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_page);
+        Button next = (Button) findViewById(R.id.speichernbtn);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Zielort.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
     }
+
+
+
+    /**
+     btnHit = (Button) findViewById(R.id.btnHit);
+    txtJson = (TextView) findViewById(R.id.tvJsonItem);
+
+btnHit.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            new JsonTask().execute("Url address here");
+        }
+    });
+**/
 }
---
+++ app/src/main/java/com/example/myflight/Zielort.java
@@ -0,0 +1,12 @@
+package com.example.myflight;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Zielort extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.zielort);
+    }
+}
