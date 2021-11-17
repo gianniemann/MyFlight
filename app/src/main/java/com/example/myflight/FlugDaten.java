d4a36c8aad04690f4850af9d88f7143bd3a79ff2
gianniemann <62234777+gianniemann@users.noreply.github.com>
2021-11-17 16:30:43 +0100

    Added FlugDaten

--- app/src/main/java/com/example/myflight/FlugDaten.java
+++ app/src/main/java/com/example/myflight/FlugDaten.java
@@ -1,5 +1,13 @@
 package com.example.myflight;
 
 public class FlugDaten {
-
+    private int id;
+    private String cityDE;
+    private String std;
+    private String etd;
+    private String statusTextDE;
+    private String flc;
+    private String fln;
+    private String airline;
+    private String typ;
 }
