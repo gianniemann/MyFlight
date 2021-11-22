e661ec44ff10b24a9c509b96490e0aa06b8dfc16
noa.kli <klinglern@bzz.ch>
2021-11-22 14:37:45 +0100

    verlink

--- app/src/main/java/com/example/myflight/Zielort.java
+++ app/src/main/java/com/example/myflight/Zielort.java
@@ -1,7 +1,11 @@
 package com.example.myflight;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.orm.SugarContext;
@@ -42,6 +46,35 @@ public class Zielort extends Activity {
             willkommen.setText(text);
         }
 
+        Button suchen = (Button) findViewById(R.id.button3);
+
+        suchen.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                EditText zielortFeld = (EditText) findViewById(R.id.textInputEditText);
+                String zielort = zielortFeld.getText().toString();
+
+                Bundle bundle = new Bundle();
+                bundle.putString("ZielOrt", zielort);
+
+                Intent myIntent = new Intent(view.getContext(), Abfluege.class);
+                myIntent.putExtras(bundle);
+                startActivity(myIntent);
+            }
+        });
+
+
+        Button abmelden = (Button) findViewById(R.id.button2);
+
+        abmelden.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+                BenutzerDAO benutzerLoeschen = new BenutzerDAO();
+                benutzerLoeschen.deleteBenutzer();
+
+                Intent myIntent = new Intent(view.getContext(), MainActivity.class);
+                startActivity(myIntent);
+            }
+        });
 
     }
 
